9bffd6126d10af383bed4773634bbe3853bf91df
YepNopeSupBye <rmurphy.25@acsamman.edu.jo>
2023-04-28 13:23:32 +0300

    Finsihed code

---
+++ Music_Finder.py
@@ -0,0 +1,119 @@
+
+import datetime
+
+
+
+Starboy = {'Length': datetime.time(0,3,30) , 'Artist':'Weeknd' , 'Genre': 'Pop'}
+I_Kissed_A_Girl = {'Length': datetime.time(0,3,00)  , 'Artist':'Katy Perry' , 'Genre': 'Pop'}
+Highway_To_Hell = {'Length': datetime.time(0,3,27) , 'Artist':'ACDC' , 'Genre': 'Hard Rock'}
+Bohemian_Rhapsody= {'Length': datetime.time(0,5,55)  , 'Artist':'Queen' , 'Genre': 'Rock'}
+Pimp = {'Length': datetime.time(0,4,9) , 'Artist':'50 Cent' , 'Genre': 'Rap'}
+Drive = {'Length': datetime.time(0,3,52)  , 'Artist':'Incubus' , 'Genre': ' alternative rock'}
+Blietzgrieg_bop = {'Length': datetime.time(0,2,12) , 'Artist':'Ramones' , 'Genre': 'Punk Rock'}
+Pachelbel_Canon_and_Gigue_for_3_Violins = {'Length': datetime.time(0,5,51)  , 'Artist':'Johann Pachelbel' , 'Genre': 'classical'}
+Stacys_Mom = {'Length': datetime.time(0,3,19) , 'Artist':'Fountains of Wayne' , 'Genre': 'Pop-Punk'}
+Drop_Stop_Roll = {'Length': datetime.time(0,2,50)  , 'Artist':'Rainbow kitten Suprise' , 'Genre': 'inde'}
+Crazy = {'Length': datetime.time(0,3,11) , 'Artist':'Domenic Haynes' , 'Genre': 'Chill'}
+Maple_syurp = {'Length': datetime.time(0,4,3)  , 'Artist':'Backseat Lovers' , 'Genre': 'Indie'}
+ParkLife = {'Length': datetime.time(0,3,7) , 'Artist':'Blur' , 'Genre': 'alternative '}
+Trust = {'Length': datetime.time(0,2,34)  , 'Artist':'Brent Faiyaz' , 'Genre': 'R&B'}
+Jolene = {'Length': datetime.time(0,2,42) , 'Artist':'Dolly Parton' , 'Genre': 'Country'}
+Buddy_Holly = {'Length': datetime.time(0,2,39)  , 'Artist':'Weezer' , 'Genre': 'Alternative'}
+Glue_Song = {'Length': datetime.time(0,2,16) , 'Artist':'Beabadoobee' , 'Genre': 'Indie'}
+Song_2 = {'Length': datetime.time(0,2,2)  , 'Artist':'Blue' , 'Genre': 'Rock'}
+Lonely_boy = {'Length': datetime.time(0,3,13) , 'Artist':'The Black Keys' , 'Genre': 'Alternative'}
+Boys_Will_Be_Bugs = {'Length': datetime.time(0,5,30)  , 'Artist':'Cave Town' , 'Genre': 'Indie'}
+Girlfriend = {'Length': datetime.time(0,3,37) , 'Artist':'Avril Lavigne' , 'Genre': 'Pop-Punk'}
+As_Long_as_You_Love_Me = {'Length': datetime.time(0,3,40)  , 'Artist':'Backstreet Boys' , 'Genre': 'Pop'}
+Wraith = {'Length': datetime.time(0,2,27) , 'Artist':'Chance the Rapper' , 'Genre': 'Rap'}
+One_Beer = {'Length': datetime.time(0,4,18) , 'Artist':'MF Doom' , 'Genre': 'Rap'}
+Solitaires = {'Length': datetime.time(0,3,25) , 'Artist':'Future' , 'Genre': 'Rap'}
+Dogtooth = {'Length': datetime.time(0,2,40) , 'Artist':'Tyler The Creator' , 'Genre': 'Rap'}
+Baby_Keem = {'Length': datetime.time(0,2,7) , 'Artist':'Baby Keem' , 'Genre': 'Rap'}
+Righteous = {'Length': datetime.time(0,4,1) , 'Artist':'Juice WRLD' , 'Genre': 'Rap'}
+Self_Medication = {'Length': datetime.time(0,5,14) , 'Artist':'Logic' , 'Genre': 'Rap'}
+My_Way = {'Length': datetime.time(0,3,38)  , 'Artist':'Calvin Harris' , 'Genre': 'Pop'}
+Where_Is_The_Love= {'Length': datetime.time(0,4,31)  , 'Artist':'Black Eyed Peas' , 'Genre': 'Pop'}
+Locked_Out_Of_Heaven = {'Length': datetime.time(0,3,52)  , 'Artist':'Bruno Mars' , 'Genre': 'Pop'}
+Dont = {'Length': datetime.time(0,3,39)  , 'Artist':'Ed Sheeran' , 'Genre': 'Pop'}
+Stay_with_Me = {'Length': datetime.time(0,2,52)  , 'Artist':'Sam Smith' , 'Genre': 'Pop'}
+Im_Yours = {'Length': datetime.time(0,4,2)  , 'Artist':'Jason Mraz' , 'Genre': 'Pop'}
+Youth = {'Length': datetime.time(0,3,4)  , 'Artist':'Troye Sivan' , 'Genre': 'Pop'}
+The_Rock_Show = {'Length': datetime.time(0,2,52) , 'Artist':'Blink 182' , 'Genre': 'Rock'}
+Cops_Dogs = {'Length': datetime.time(0,2,00) , 'Artist':'Destructo Disk' , 'Genre': 'Rock'}
+Shadows_Follow = {'Length': datetime.time(0,6,11) , 'Artist':'Metallica' , 'Genre': 'Rock'}
+Boys_Dont_Cry = {'Length': datetime.time(0,2,35) , 'Artist':'The Cure' , 'Genre': 'Rock'}
+A_Horse_with_No_Name = {'Length': datetime.time(0,4,12) , 'Artist':'America' , 'Genre': 'Rock'}
+Piano_Man = {'Length': datetime.time(0,5,38) , 'Artist':'Billy Joel' , 'Genre': 'Rock'}
+Should_I_Stay_or_Should_I_Go = {'Length': datetime.time(0,3,8) , 'Artist':'The Clash' , 'Genre': 'Rock'}
+Irish_Wiskey_On_The_Shelf = {'Length': datetime.time(0,2,52)  , 'Artist':'Lee Matthews' , 'Genre': 'Country'}
+Last_Night = {'Length': datetime.time(0,2,42)  , 'Artist':'Morgan Wallen' , 'Genre': 'Country'}
+Fast_Car = {'Length': datetime.time(0,4,25)  , 'Artist':'Luke Combs' , 'Genre': 'Country'}
+Shot_Of_Glory = {'Length': datetime.time(0,3,38)  , 'Artist':'Barry Kirwan' , 'Genre': 'Country'}
+Star_Of_The_County_Down = {'Length': datetime.time(0,2,53)  , 'Artist':'The Ennis Brothers' , 'Genre': 'Country'}
+Tennessee_Whiskey = {'Length': datetime.time(0,4,52)  , 'Artist':'Chris Stapleton' , 'Genre': 'Country'}
+Life_Is_A_Highway = {'Length': datetime.time(0,4,36)  , 'Artist':'Rascal Flatts' , 'Genre': 'Country'}
+Big_Jet_Plane = {'Length': datetime.time(0,2,28) , 'Artist':'Restricted' , 'Genre': 'Techno'}
+Meet_Her_at_The_Love_Parade = {'Length': datetime.time(0,1,52) , 'Artist':'Vieze Asbak' , 'Genre': 'Techno'}
+Golden_SZN = {'Length': datetime.time(0,4,53) , 'Artist':'OGUZ' , 'Genre': 'Techno'}
+Falkors_Return = {'Length': datetime.time(0,3,00) , 'Artist':'Indecroum' , 'Genre': 'Techno'}
+Hypnosis = {'Length': datetime.time(0,2,38) , 'Artist':'AYYBO' , 'Genre': 'Techno'}
+Liverpool_street_in_the_Rain = {'Length': datetime.time(0,5,32) , 'Artist':'Mall Grab' , 'Genre': 'Techno'}
+Colonia = {'Length': datetime.time(0,3,54) , 'Artist':'Space 92' , 'Genre': 'Techno'}
+Dark_Red = {'Length': datetime.time(0,2,52)  , 'Artist':'Steve Lacy' , 'Genre': 'Indie'}
+Hell_N_Back = {'Length': datetime.time(0,3,33)  , 'Artist':'Bakar' , 'Genre': 'Indie'}
+Its_Called_Freefall = {'Length': datetime.time(0,2,31)  , 'Artist':'Rainbow Kitten Suprise' , 'Genre': 'Indie'}
+Babydoll = {'Length': datetime.time(0,1,37)  , 'Artist':'Dominic Fike' , 'Genre': 'Indie'}
+Bad_Habit = {'Length': datetime.time(0,3,51)  , 'Artist':'Steve Lacy' , 'Genre': 'Indie'}
+Heart_to_Heart = {'Length': datetime.time(0,3,30)  , 'Artist':'Mac DeMarco' , 'Genre': 'Indie'}
+Love_It_If_We_Made_It = {'Length': datetime.time(0,4,12)  , 'Artist':'The 1975' , 'Genre': 'Indie'}
+ddu_DU_DDU_DU = {'Length': datetime.time(0,3,35) , 'Artist':'Black Pink' , 'Genre': 'K-Pop'}
+Omg= {'Length': datetime.time(0,3,32) , 'Artist':'New Jeans' , 'Genre': 'K-Pop'}
+flower = {'Length': datetime.time(0,2,46) , 'Artist':'JISOO' , 'Genre': 'K-Pop'}
+Cupid = {'Length': datetime.time(0,2,40) , 'Artist':'FIFTY FIFTY' , 'Genre': 'K-Pop'}
+Sour_Sweet = {'Length': datetime.time(0,2,23) , 'Artist':'BamBam' , 'Genre': 'K-Pop'}
+Like_Crazy = {'Length': datetime.time(0,3,25) , 'Artist':'Jimin' , 'Genre': 'K-Pop'}
+Rock_With_You = {'Length': datetime.time(0,3,32) , 'Artist':'Jay Park' , 'Genre': 'K-Pop'}
+Feather = {'Length': datetime.time(0,2,52) , 'Artist':'Nujabes' , 'Genre': 'Jazz'}
+ZipLock = {'Length': datetime.time(0,3,18) , 'Artist':'Saba' , 'Genre': 'Jazz'}
+Terrapin = {'Length': datetime.time(0,2,53) , 'Artist':'Ben Beal' , 'Genre': 'Jazz'}
+Ina_Crueler = {'Length': datetime.time(0,3,33) , 'Artist':'Bricknasty' , 'Genre': 'Jazz'}
+Out_Of_Wilma = {'Length': datetime.time(0,2,49) , 'Artist':'Annie Berenson' , 'Genre': 'Jazz'}
+Fools_Rush_In = {'Length': datetime.time(0,4,33) , 'Artist':'the Southern Belle' , 'Genre': 'Jazz'}
+Mysterious_Stranger = {'Length': datetime.time(0,3,53) , 'Artist':'6th Street' , 'Genre': 'Jazz'}
+Drunk_Dial = {'Length': datetime.time(0,2,28)  , 'Artist':'Sueco' , 'Genre': 'Punk'}
+El_Dorado = {'Length': datetime.time(0,3,55)  , 'Artist':'Prince Daddy' , 'Genre': 'Punk'}
+Manic_Man = {'Length': datetime.time(0,2,14)  , 'Artist':'Curtis Waters' , 'Genre': 'Punk'}
+Eco_Anxiety = {'Length': datetime.time(0,2,37)  , 'Artist':'Oli Frost' , 'Genre': 'Punk'}
+Delia_Smith = {'Length': datetime.time(0,3,38)  , 'Artist':'Sprints' , 'Genre': 'Punk'}
+Teach_Me_How_To_Dance = {'Length': datetime.time(0,4,36)  , 'Artist':'The Queens head' , 'Genre': 'Punk'}
+Ok = {'Length': datetime.time(0,3,53)  , 'Artist':'Celopatrick' , 'Genre': 'Punk'}
+Crazy_blues = {'Length': datetime.time(0,3,22) , 'Artist':'Mamie Smith' , 'Genre': 'Blues'}
+Ash_tray_Blues = {'Length': datetime.time(0,2,58) , 'Artist':'Papa Charlie Jakson' , 'Genre': 'Blues'}
+Devil_Got_My_Woman = {'Length': datetime.time(0,3,1) , 'Artist':'Skip James' , 'Genre': 'Blues'}
+All_Around_Man = {'Length': datetime.time(0,3,00) , 'Artist':'Bo Carter' , 'Genre': 'Blues'}
+You_Was_Born_To_Die = {'Length': datetime.time(0,2,51) , 'Artist':'Curley Weaver' , 'Genre': 'Blues'}
+Cross_Road_Blues = {'Length': datetime.time(0,2,29) , 'Artist':'Robert Johnson' , 'Genre': 'Blues'}
+Macth_Box_Blues = {'Length': datetime.time(0,3,3) , 'Artist':'Blind Lemon Jefferson' , 'Genre': 'Blues'}
+Dont_Call_Me_Baby = {'Length': datetime.time(0,3,48)  , 'Artist':'Madison Avenue' , 'Genre': 'Irish_House'}
+Baby_Dont_hurt_Me = {'Length': datetime.time(0,2,19) , 'Artist':'David Guetta' , 'Genre': 'Dance'}
+Giving_Me = {'Length': datetime.time(0,2,48)  , 'Artist':'Jazzy' , 'Genre': 'Dance'}
+Ramadan = {'Length': datetime.time(0,4,7) , 'Artist':'Maher Zain' , 'Genre': 'Arab'}
+Tu_Con_El = {'Length': datetime.time(0,5,00)  , 'Artist':'frankie Ruiz' , 'Genre': 'Salsa'}
+Make_You= {'Length': datetime.time(0,3,00) , 'Artist':'Sleepy Hallow' , 'Genre': 'David'}
+Yoru_Ni_Kakeru = {'Length': datetime.time(0,4,21)  , 'Artist':'Yoasobi' , 'Genre': 'J-pop'}
+Night_dancer = {'Length': datetime.time(0,3,31) , 'Artist':'imase' , 'Genre': 'J-pop'}
+
+songs = [Starboy, I_Kissed_A_Girl, Highway_To_Hell, Bohemian_Rhapsody, Pimp, Drive, Blietzgrieg_bop, Pachelbel_Canon_and_Gigue_for_3_Violins, Stacys_Mom, Drop_Stop_Roll, Crazy, Maple_syurp, ParkLife, Trust, Jolene, Buddy_Holly, Glue_Song, Song_2, Lonely_boy, Boys_Will_Be_Bugs, Girlfriend, As_Long_as_You_Love_Me, Wraith, One_Beer, Solitaires, Dogtooth, Baby_Keem, Righteous, Self_Medication, My_Way, Where_Is_The_Love, Locked_Out_Of_Heaven, Dont, Stay_with_Me, Im_Yours, Youth, The_Rock_Show, Cops_Dogs, Shadows_Follow, Boys_Dont_Cry, A_Horse_with_No_Name, Piano_Man, Should_I_Stay_or_Should_I_Go, Irish_Wiskey_On_The_Shelf, Last_Night, Fast_Car, Shot_Of_Glory, Star_Of_The_County_Down, Tennessee_Whiskey, Life_Is_A_Highway, Big_Jet_Plane, Meet_Her_at_The_Love_Parade, Golden_SZN, Falkors_Return, Hypnosis, Liverpool_street_in_the_Rain, Colonia, Dark_Red, Hell_N_Back, Its_Called_Freefall, Babydoll, Bad_Habit, Heart_to_Heart, Love_It_If_We_Made_It, ddu_DU_DDU_DU, Omg, flower, Cupid, Sour_Sweet, Like_Crazy, Rock_With_You, Feather, ZipLock, Terrapin, Ina_Crueler, Out_Of_Wilma, Fools_Rush_In, Mysterious_Stranger, Drunk_Dial, El_Dorado, Manic_Man, Eco_Anxiety, Delia_Smith, Teach_Me_How_To_Dance, Ok, Crazy_blues, Ash_tray_Blues, Devil_Got_My_Woman, All_Around_Man, You_Was_Born_To_Die, Cross_Road_Blues, Macth_Box_Blues, Dont_Call_Me_Baby, Baby_Dont_hurt_Me, Giving_Me, Ramadan, Tu_Con_El, Make_You, Yoru_Ni_Kakeru, Night_dancer]
+songs_length = [Starboy['length'], I_Kissed_A_Girl['length'], Highway_To_Hell, Bohemian_Rhapsody, Pimp, Drive, Blietzgrieg_bop, Pachelbel_Canon_and_Gigue_for_3_Violins, Stacys_Mom, Drop_Stop_Roll, Crazy, Maple_syurp, ParkLife, Trust, Jolene, Buddy_Holly, Glue_Song, Song_2, Lonely_boy, Boys_Will_Be_Bugs, Girlfriend, As_Long_as_You_Love_Me, Wraith, One_Beer, Solitaires, Dogtooth, Baby_Keem, Righteous, Self_Medication, My_Way, Where_Is_The_Love, Locked_Out_Of_Heaven, Dont, Stay_with_Me, Im_Yours, Youth, The_Rock_Show, Cops_Dogs, Shadows_Follow, Boys_Dont_Cry, A_Horse_with_No_Name, Piano_Man, Should_I_Stay_or_Should_I_Go, Irish_Wiskey_On_The_Shelf, Last_Night, Fast_Car, Shot_Of_Glory, Star_Of_The_County_Down, Tennessee_Whiskey, Life_Is_A_Highway, Big_Jet_Plane, Meet_Her_at_The_Love_Parade, Golden_SZN, Falkors_Return, Hypnosis, Liverpool_street_in_the_Rain, Colonia, Dark_Red, Hell_N_Back, Its_Called_Freefall, Babydoll, Bad_Habit, Heart_to_Heart, Love_It_If_We_Made_It, ddu_DU_DDU_DU, Omg, flower, Cupid, Sour_Sweet, Like_Crazy, Rock_With_You, Feather, ZipLock, Terrapin, Ina_Crueler, Out_Of_Wilma, Fools_Rush_In, Mysterious_Stranger, Drunk_Dial, El_Dorado, Manic_Man, Eco_Anxiety, Delia_Smith, Teach_Me_How_To_Dance, Ok, Crazy_blues, Ash_tray_Blues, Devil_Got_My_Woman, All_Around_Man, You_Was_Born_To_Die, Cross_Road_Blues, Macth_Box_Blues, Dont_Call_Me_Baby, Baby_Dont_hurt_Me, Giving_Me, Ramadan, Tu_Con_El, Make_You, Yoru_Ni_Kakeru, Night_dancer]
+
+def Get_Time():
+    g
+def Get_Artist():
+    g
+def Get_Genre():
+
+
+
+
+
